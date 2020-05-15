6c93728bbe8c4cc60fb5c0ce4ae9401886eb3c89
yuta asato <yutaasato@yahoo.com>
2020-05-15 13:01:53 +0900

    fixing knex connection

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl:true,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
