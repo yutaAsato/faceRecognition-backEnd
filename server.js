6dbb6cfff0ec9994d87cacbfaab77fde607aa77d
yuta asato <yutaasato@yahoo.com>
2020-06-23 22:25:45 +0900

    ficed local issue

--- server.js
+++ server.js
@@ -1,85 +1,92 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bcrypt = require('bcrypt-nodejs')
+const bcrypt = require("bcrypt-nodejs");
 
-const cors = require('cors')
+const cors = require("cors");
 
 //connects this server to psql databse
-const knex = require('knex')
+const knex = require("knex");
 
+//for heroku----------
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+//for local----------
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-  });
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "oblivion",
+    database: "facerecognition",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .where({ id: 2 })
+  .then((res) => console.log(res));
 
 //---------importing from controllers folder--------
 
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profileId = require('./controllers/profileId')
-const image = require('./controllers/image')
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profileId = require("./controllers/profileId");
+const image = require("./controllers/image");
 
 //------------------
 
-
-
-
 //-----middleware--
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 //-----------
 
-
-
-
-
-
-
-app.get('/', (req, res) => {res.send("it is working!")})   
-
+app.get("/", (req, res) => {
+  res.send("it is working!");
+});
 
 //singin
-app.post('/signin', (req, res) => { signin.handleSignin (req, res, db, bcrypt)})
-   
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
 
 //register
-app.post('/register',(req, res) => { register.handleRegister( req, res, db, bcrypt)})
-
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
 //profileId
-app.get('/profile/:id', (req, res) => { profileId.handleProfileId(req, res,db)})
+app.get("/profile/:id", (req, res) => {
+  profileId.handleProfileId(req, res, db);
+});
 
-   
 //image(update entries)
-app.put('/image',(req, res) => {image.handleImage(req,res, db)})
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
 
 //handleAPI (Clarifai)
-app.post('/imageurl',(req, res) => {image.handleApi(req,res,)})
-
- 
-
-
-
-
-
-
-
+app.post("/imageurl", (req, res) => {
+  image.handleApi(req, res);
+});
 
 //-------LISTEN--------
-app.listen(process.env.PORT || 3000, () =>{
-    console.log(`running on ${process.env.PORT}`)
-})
 
-
+// for heroku------
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`running on ${process.env.PORT}`);
+});
 
 /*
 /--> res = this is working
