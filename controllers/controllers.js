0cfc2174b4d642de817e2026997c27a224258346
Yuta Asato <yuta@Yutas-MacBook-Pro.local>
2021-09-12 10:12:20 +0900

    update Clarifai endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -18,7 +18,7 @@ const app = new Clarifai.App({
 
 exports.handleApi = (req, res) => {
   app.workflow
-    .predict("Demographics", req.body.input)
+    .predict("new-demographic", req.body.input)
     .then((data) => res.json(data))
     .catch((err) => res.status(400).json(err));
 };
